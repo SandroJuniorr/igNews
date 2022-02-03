59e32ff0b0270aca4b908b989fa3d651485f9f70
Sandro <sandrox377@gmail.com>
2022-02-03 19:22:40 -0300

    criação Next e estilos globais

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ínicio | ig.news</title>
+      </Head>
+      <p>Hellow</p>
+    </>
+  );
+}
